f70f020749678c838f10bfd8122f388ee1fd224b
Mitchell Sun <mingchenothin9@gmail.com>
2023-10-20 13:54:36 +1100

    fix: missing page in secondary nav menu

--- src/layouts/Header/navBarConfig.ts
+++ src/layouts/Header/navBarConfig.ts
@@ -23,6 +23,10 @@ export const navItemsConfig: { [K in ENavItem]: INavItem } = {
 export const subNavItemsConfig: { [K in ENavItem]?: INavItem[] } = {
 	[ENavItem.ABOUT]: [
 		{
+			title: 'Stage',
+			path: '/stages'
+		},
+		{
 			title: 'Venue',
 			path: '/venue'
 		}
