c974312e918025d0911b5b710deebba4cc85071a
LingHan.S <LINGKUI0324@GMAIL.COM>
2023-10-27 12:48:14 +1000

    fix css layout

--- src/pages/general-events.tsx
+++ src/pages/general-events.tsx
@@ -11,13 +11,18 @@ const Cover = styled.div`
 	background-image: url('/images/background/generalEventsHeaderBG.png');
 	background-position: center;
 	background-size: cover;
-
 	@media ${devices.miniMobile} {
 		height: 478px;
 	}
 	@media ${devices.largerLaptop} {
 		height: 360px;
 	}
+`;
+
+const PageCoverTitleContainer = styled.div`
+	font-family: Arial;
+	letter-spacing: 0.78px;
+	margin-top: 104px;
 `;
 
 const GeneralEventsPage = () => {
@@ -33,7 +38,9 @@ const GeneralEventsPage = () => {
 			<PageHead meta={meta} />
 			<Cover>
 				<Header />
-				<PageCoverTitle coverText={coverText} />
+				<PageCoverTitleContainer>
+					<PageCoverTitle coverText={coverText} />
+				</PageCoverTitleContainer>
 			</Cover>
 			<h1>this is GeneralEventsPage</h1>
 		</>
