a02d824ce48b304291eee021602d08a31aae1486
LingHan.S <LINGKUI0324@GMAIL.COM>
2023-11-03 09:36:40 +1000

    Footer refac

--- src/layouts/Footer/components/FooterContent/FooterContent.tsx
+++ src/layouts/Footer/components/FooterContent/FooterContent.tsx
@@ -1,8 +1,5 @@
 import Box from '@mui/material/Box';
-import FormControl from '@mui/material/FormControl';
 import Grid from '@mui/material/Grid';
-import Input from '@mui/material/Input';
-import InputAdornment from '@mui/material/InputAdornment';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
@@ -10,12 +7,10 @@ import styled from 'styled-components';
 
 import SubscriptionModal from '@/components/Shares/SubscriptionModal';
 import useSubscription from '@/hooks/userSubscription';
-import { tagDecoration } from '@/styles/mixin';
 import { color, devices } from '@/styles/variables';
 import imageLoader from '@/utils/loader';
-import { isEmail } from '@/utils/validator';
 
-const { blackColor, primaryColor, warningColor, whiteColor } = color;
+const { blackColor, primaryColor, whiteColor } = color;
 
 // Styles
 
@@ -59,43 +54,11 @@ const ContentText = styled.p`
 	margin-top: 0;
 `;
 
-const StyledInput = styled(Input)`
-	border-bottom: 2px solid ${primaryColor};
-	color: ${whiteColor};
-	&:after {
-		display: none;
+const QRcodeGridItem = styled(Grid)`
+	margin: 0 24px 24px 0;
+	@media (${devices.miniMobile}) {
+		margin: 0 18px 6px 0;
 	}
-`;
-
-const StyledInputStartAdornment = styled(InputAdornment)`
-	p {
-		color: ${whiteColor};
-	}
-`;
-
-const StyledSubmitButton = styled.button`
-	-moz-transition: color 0.5s;
-	-webkit-transition: color 0.5s;
-	background-color: transparent;
-	border: none;
-	color: ${whiteColor};
-	cursor: pointer;
-	padding: 0 4px;
-	transition: color 0.5s;
-	&:hover {
-		color: ${primaryColor};
-	}
-	span {
-		text-transform: uppercase;
-		${tagDecoration};
-	}
-`;
-
-const EmailHelperText = styled.p`
-	color: ${warningColor};
-	margin: 0;
-	position: absolute;
-	top: 100%;
 `;
 
 // Configuration
@@ -122,28 +85,11 @@ const linkConfigList = [
 const FooterContent = () => {
 	const [emailInput, setEmailInput] = useState<string>('');
 	const [inputMessage, setInputMessage] = useState<string>('');
-	const { message, openModal, setOpenModal, submitEmail } = useSubscription(
-		emailInput,
-		setEmailInput
-	);
+	const { message, openModal, setOpenModal } = useSubscription(emailInput, setEmailInput);
 
 	useEffect(() => {
 		if (inputMessage) setInputMessage('');
 	}, [emailInput]);
-
-	const handleSubmitEmail = () => {
-		if (!isEmail(emailInput)) {
-			setInputMessage('Email is invalid.');
-		} else {
-			submitEmail();
-		}
-	};
-
-	const handleKeyPress = (e: React.KeyboardEvent<HTMLElement>) => {
-		if (e.key === 'Enter') {
-			handleSubmitEmail();
-		}
-	};
 
 	const handleClose = () => {
 		setOpenModal(false);
@@ -151,7 +97,7 @@ const FooterContent = () => {
 
 	return (
 		<ContentContainer sx={{ flexGrow: 1 }} className="flex justify-center">
-			<ContainerGrid container spacing={4}>
+			<ContainerGrid container spacing={3}>
 				<Grid item mobile={12} tablet={6} laptop={3} largeLaptop={3}>
 					<Logo
 						loader={imageLoader}
@@ -180,31 +126,21 @@ const FooterContent = () => {
 						))}
 					</div>
 				</Grid>
-				<Grid item mobile={12} tablet={6} laptop={3} largeLaptop={3} />
 				<Grid item mobile={12} tablet={6} laptop={3} largeLaptop={3}>
-					<Title>Join the Newsletter</Title>
-					<ContentText>Stay informed about the latest events.</ContentText>
-					<FormControl fullWidth variant="outlined" className="relative">
-						<StyledInput
-							value={emailInput}
-							onChange={event => setEmailInput(event.target.value)}
-							onFocus={() => setInputMessage('')}
-							onKeyDown={handleKeyPress}
-							startAdornment={
-								<StyledInputStartAdornment position="start">
-									Email*
-								</StyledInputStartAdornment>
-							}
-							endAdornment={
-								<InputAdornment position="end">
-									<StyledSubmitButton onClick={handleSubmitEmail}>
-										<span>Send</span>
-									</StyledSubmitButton>
-								</InputAdornment>
-							}
-						/>
-						{inputMessage && <EmailHelperText>{inputMessage}</EmailHelperText>}
-					</FormControl>
+					<Title>Scan to connect</Title>
+					<Grid container>
+						{[1, 2, 3, 4, 5, 6].map(i => (
+							<QRcodeGridItem item mobile={3} tablet={3} laptop={3} largeLaptop={3}>
+								<Image
+									loader={imageLoader}
+									src={`/images/qr-code/${i}.png`}
+									alt={`Image ${i}`}
+									width={86}
+									height={86}
+								/>
+							</QRcodeGridItem>
+						))}
+					</Grid>
 				</Grid>
 			</ContainerGrid>
 			<SubscriptionModal message={message} open={openModal} handleClose={handleClose} />
