50e50bc2ae6f9aa2d9c5c58fdd894e16c89d10e5
Regan Leung <leungregan2021@gmail.com>
2023-11-23 19:38:49 +1100

    rename generalevent folder to sideevnet folder and fixed api with sideevent data

--- src/services/meetup.ts
+++ src/services/meetup.ts
@@ -12,8 +12,14 @@ export const fetchFeaturedEvents = () => {
 	return request({
 		baseURL,
 		method: 'GET',
-		url: '/api/meetups?meetupEventType=web3&featured=generalEvent&state=published'
+		url: '/api/meetups?meetupEventType=web3&web3EventType=sideEvent&state=published&featured=popularEvents'
 	});
 };
 
-export default fetchMeetups;
+export const fetchMoreEvents = () => {
+	return request({
+		baseURL,
+		method: 'GET',
+		url: '/api/meetups?meetupEventType=web3&web3EventType=sideEvent&state=published&featured=default'
+	});
+};
