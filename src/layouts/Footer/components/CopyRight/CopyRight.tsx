c00d9c820649438f9a1139b1e1a253affaf62427
Noah <tayeshi1010@gmail.com>
2023-11-27 10:06:18 +0800

    update-footer

--- src/layouts/Footer/components/CopyRight/CopyRight.tsx
+++ src/layouts/Footer/components/CopyRight/CopyRight.tsx
@@ -7,40 +7,46 @@ import { color, devices, sizes } from '@/styles/variables';
 import imageLoader from '@/utils/loader';
 
 const { primaryColor } = color;
-
-const LogoContainer = styled.div`
-	width: 20vw;
-`;
-const CopyRightContainer = styled.section`
+const ContentContainer = styled.section`
 	align-items: center;
 	background-color: ${primaryColor};
-	padding: 8px 0;
+	font-size: 14px;
+	padding: 12px 0;
 `;
-const ContentContainer = styled.div`
-	align-items: center;
-	display: flex;
-	justify-content: space-between;
+const ContainerGrid = styled(Grid)`
 	margin: 0 auto;
 	width: calc(100vw - 75px);
 	@media (${devices.largeLaptop}) {
 		max-width: 1440px;
 	}
 `;
-
 const Logo = styled(Image)`
 	margin-bottom: 16px;
-
 	@media ${devices.tablet} {
 		margin-bottom: 0;
 	}
 `;
-
+const CopyrightGrid = styled(Grid)`
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	min-width: '600px';
+`;
 const CopyrightText = styled.p`
+	margin: 0 0;
 	@media ${devices.largeLaptop} {
 		white-space: nowrap;
 	}
 `;
-
+const gridColumn = {
+	mobile: 12,
+	tablet: 12,
+	laptop: 3
+};
+const EmptyBox = styled.div`
+	width: 120px;
+`;
 const CopyRight: React.FC = () => {
 	const isMobileWidth = typeof window !== 'undefined' && window.innerWidth <= sizes.tablet;
 	const [isMobile, setIsMobile] = useState(isMobileWidth);
@@ -49,37 +55,47 @@ const CopyRight: React.FC = () => {
 			setIsMobile(window.innerWidth <= sizes.tablet);
 		};
 		window.addEventListener('resize', resizeHandler);
-
 		return () => window.removeEventListener('resize', resizeHandler);
 	}, []);
-
 	return (
-		<CopyRightContainer>
-			<ContentContainer>
-				<div>
-					<LogoContainer>
-						<Logo
-							loader={imageLoader}
-							unoptimized
-							src="/web3-logo.svg"
-							alt="logo"
-							width={120}
-							height={40}
-							priority
-						/>
-					</LogoContainer>
-				</div>
-				<div>
-					<CopyrightText>
-						Copyright © 2023-2024 Web3 Convention, All Rights Reserved. ACN 668 836 566
-					</CopyrightText>
-				</div>
-				<div>
-					<CopyrightText>Terms And Conditions | Anti-harassment Policy</CopyrightText>
-				</div>
-			</ContentContainer>
-		</CopyRightContainer>
+		<ContentContainer>
+			<ContainerGrid container className="justify-between">
+				<Grid item {...gridColumn} className="self-center">
+					<Logo
+						loader={imageLoader}
+						unoptimized
+						src="/web3-logo.svg"
+						alt="logo"
+						width={120}
+						height={40}
+						priority
+					/>
+				</Grid>
+				<CopyrightGrid item {...gridColumn}>
+					{isMobile ? (
+						<>
+							<CopyrightText>© 2023 AI+Web3 Convention</CopyrightText>
+							<CopyrightText>All Rights Reserved</CopyrightText>
+							<CopyrightText>ACN 668 836 566</CopyrightText>
+							<CopyrightText>Terms And Conditions</CopyrightText>
+							<CopyrightText>Anti-harassment Policy</CopyrightText>
+						</>
+					) : (
+						<>
+							<CopyrightText>
+								© 2023 AI+Web3 Convention, All Rights Reserved. ACN 668 836 566
+							</CopyrightText>
+							<CopyrightText>
+								Terms And Conditions | Anti-harassment Policy
+							</CopyrightText>
+						</>
+					)}
+				</CopyrightGrid>
+				<Grid item {...gridColumn}>
+					<EmptyBox />
+				</Grid>
+			</ContainerGrid>
+		</ContentContainer>
 	);
 };
-
 export default CopyRight;
