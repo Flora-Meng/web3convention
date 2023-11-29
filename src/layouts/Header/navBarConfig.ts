0a4dcf173f660f310290366effb2979072e2f608
Noah <tayeshi1010@gmail.com>
2023-11-29 14:45:39 +0800

    初始化partners页面

--- src/layouts/Header/navBarConfig.ts
+++ src/layouts/Header/navBarConfig.ts
@@ -77,7 +77,8 @@ export const subNavItemsConfig: { [K in ENavItem]?: INavItem[] } = {
 	[ENavItem.PARTNER]: [
 		{
 			title: 'Partner with Us',
-			path: 'https://w3con.eventsair.com/SponsorshipPortal/Account/Login?ReturnUrl=%2FSponsorshipPortal%2Fweb3convention%2Fsponsorship'
+			// path: 'https://w3con.eventsair.com/SponsorshipPortal/Account/Login?ReturnUrl=%2FSponsorshipPortal%2Fweb3convention%2Fsponsorship'
+			path: '/partners'
 		},
 		{
 			title: 'Why Partner?',
