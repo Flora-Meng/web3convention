61f4bda3982b2dfa8148dbc67ee6825b04792ed3
Kehan Zhang <zhangkehan0712@gmail.com>
2023-12-14 10:36:37 +0800

    update solution of difference

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import JudgeTeam from '@/components/Pages/Hackathon/JudgeTeam';
 import HomeCarousel from '@/components/Pages/Home/HomeCarousel';
 import HomeCountdown from '@/components/Pages/Home/HomeCountdown';
 import HomeCounter from '@/components/Pages/Home/HomeCounter';
 import HomeEvent from '@/components/Pages/Home/HomeEvent';
 import HomeHighLights from '@/components/Pages/Home/HomeHighLights/HomeHighLights';
-import HomeJudgeTeam from '@/components/Pages/Home/HomeJudgeTeam';
 // import HomeGallery from '@/components/Pages/Home/HomeGallery';
+import teamMemberListHome from '@/components/Pages/Home/HomeJudgeTeam/teamMemberList.json';
 import HomePosts from '@/components/Pages/Home/HomePosts';
 import HomeSponsor from '@/components/Pages/Home/HomeSponsor';
 // import HomeSubscription from '@/components/Pages/Home/HomeSubscription';
@@ -34,7 +35,7 @@ const Homepage: ComponentType = () => {
 			<HomeHighLights />
 			<PageBanner bannerInfo={bannerInfo} buttonConfig={buttonConfig} />
 			<HomeEvent />
-			<HomeJudgeTeam />
+			<JudgeTeam subtitle="AI + Web3 Convention 2024" teamMemberList={teamMemberListHome} />
 			<PageTicket />
 			<HomeCounter />
 			{/* <HomeSubscription /> */}
