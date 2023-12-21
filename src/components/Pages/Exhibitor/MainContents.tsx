fb15b2579168adc8e453ca15cf93e16ca789f724
Regan Leung <leungregan2021@gmail.com>
2023-12-21 18:20:31 +1100

    add detail info section

--- src/components/Pages/Exhibitor/MainContents.tsx
+++ src/components/Pages/Exhibitor/MainContents.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+import ExhibitorDetailedInformation from './ExhibitorDetailedInformation';
 import fetchExhibitorById from '@/services/companies';
 import { color, devices } from '@/styles/variables';
 
@@ -10,11 +11,18 @@ const { blackColor, primaryColor, whiteColor } = color;
 const Container = styled.div`
 	background-color: ${blackColor};
 `;
+type IExhibitor = {
+	staffAmount: number;
+	website: string;
+	email: string;
+	industry: string;
+	officeAddress: string;
+};
 
 const MainContents = () => {
 	const router = useRouter();
 	const { exhibitorId } = router.query;
-	const [exhibitorInfo, setExhibitorInfo] = useState({});
+	const [exhibitorInfo, setExhibitorInfo] = useState<IExhibitor | null>(null);
 	const fetchData = async () => {
 		if (typeof exhibitorId === 'string') {
 			const response = await fetchExhibitorById(exhibitorId);
@@ -24,8 +32,12 @@ const MainContents = () => {
 
 	useEffect(() => {
 		fetchData();
-	}, []);
-	return <Container>asdas</Container>;
+	}, [exhibitorId]);
+	return (
+		<Container>
+			{exhibitorInfo && <ExhibitorDetailedInformation exhibitorInfo={exhibitorInfo} />}
+		</Container>
+	);
 };
 
 export default MainContents;
