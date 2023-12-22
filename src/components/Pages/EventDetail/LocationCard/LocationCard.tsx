e5ce5585c3069ad580194fef73a1b3b2d15fc460
Kehan Zhang <zhangkehan0712@gmail.com>
2023-12-22 13:04:08 +0800

    Update LocationCard.tsx

--- src/components/Pages/EventDetail/LocationCard/LocationCard.tsx
+++ src/components/Pages/EventDetail/LocationCard/LocationCard.tsx
@@ -126,9 +126,10 @@ const LocationCard = () => {
 	}, [userLocation, latitude, longitude]);
 
 	const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
-		const R = 6371;
+		const R = 6371; /* radius of earth */
 		const dLat = ((lat2 - lat1) * Math.PI) / 180;
 		const dLon = ((lon2 - lon1) * Math.PI) / 180;
+		/* variable to calculate spherical distance */
 		const a =
 			Math.sin(dLat / 2) * Math.sin(dLat / 2) +
 			Math.cos((lat1 * Math.PI) / 180) *
