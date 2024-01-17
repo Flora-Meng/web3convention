4fe6d3b7c6ee6d88163443dd4b769bfdf48e4b25
Kehan Zhang <zhangkehan0712@gmail.com>
2024-01-17 14:26:52 +0800

    delete useless part

--- src/components/Pages/Startup/Prize/Prize.tsx
+++ src/components/Pages/Startup/Prize/Prize.tsx
@@ -31,47 +31,6 @@ const Title = styled.h2`
 	${animation('fade-in-opacity-transform-to-up', '0.5s', 'ease', '0.5s', '1')};
 	${animationFillMode()};
 `;
-const StyledGridContainer = styled.div`
-	display: grid;
-	gap: 50px;
-	position: relative;
-	@media ${devices.tablet} {
-		column-gap: 40px;
-		row-gap: 60px;
-		grid-template-columns: repeat(2, 1fr);
-	}
-	@media ${devices.largeLaptop} {
-		column-gap: 60px;
-		row-gap: 66px;
-		grid-template-columns: repeat(3, 360px);
-	}
-`;
-const StyledGridItem = styled.div`
-	background-color: #131313;
-	height: 346px;
-	max-width: 368px;
-	position: relative;
-`;
-const SubTitle = styled.p`
-	background-color: #ecf652;
-	color: ${color.blackcolor};
-	font-size: 24px;
-	font-weight: bold;
-	height: 58px;
-	margin: 0 0 24px;
-	max-width: 368px;
-	padding: 17px 24px;
-`;
-const DescriptionList = styled.ul`
-	padding: 0 24px 0 36px;
-`;
-
-const DescriptionItem = styled.li`
-	color: ${color.whiteColor};
-	font-size: 16px;
-	line-height: 1.5;
-	margin-bottom: 16px;
-`;
 const SectionSubtitle = styled.p`
 	${sectionSubtitle};
 `;
