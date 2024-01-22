b746ab10ae195c57e7c7fbc8f227f81b71d23d60
Huimin Li <lihuimin699@gmail.com>
2024-01-22 12:04:40 +1100

    feature/WC3-184 Startup page Event agenda section

---
+++ src/components/Pages/Startup/EventAgenda/EventAgenda.tsx
@@ -0,0 +1,77 @@
+import Grid from '@mui/material/Grid';
+import styled from 'styled-components';
+
+import eventAgenda from './EventAgenda.json';
+import ColorfulCard from '@/components/Shares/ColorfulCard';
+import { sectionSubtitle, sectionTitle } from '@/styles/mixin';
+import { devices } from '@/styles/variables';
+
+const colors = ['#52f6c6', '#69eff2', '#f4cad4'];
+
+const EventAgendaContainer = styled.div`
+	background-color: #000;
+	padding: 24px;
+	width: 100%;
+`;
+
+const ContentContainer = styled.div`
+	margin: 96px auto;
+	max-width: 1440px;
+	width: 100%;
+	@media ${devices.tablet} {
+		padding: 0 40px;
+	}
+`;
+
+const Title = styled.h2`
+	${sectionTitle};
+	color: #fff;
+	margin-bottom: 40px;
+	max-width: unset;
+`;
+const Subtitle = styled.p`
+	${sectionSubtitle};
+`;
+
+const CardWrapper = styled.div`
+	background-color: #131313;
+	height: 466px;
+	justify-content: center;
+	padding: 0 0 24px;
+	width: 368px;
+`;
+
+const EventAgenda = () => {
+	return (
+		<EventAgendaContainer>
+			<ContentContainer>
+				<Subtitle>Web3 Hackathon 2024</Subtitle>
+				<Title>Event Agenda</Title>
+				<Grid container spacing={6} wrap="wrap">
+					{eventAgenda.map((eventAgendaInfo, index) => (
+						<Grid
+							item
+							mobile={12}
+							tablet={12}
+							laptop={6}
+							largeLaptop={4}
+							key={eventAgendaInfo.title}
+						>
+							<CardWrapper>
+								<ColorfulCard
+									title={eventAgendaInfo.title}
+									subtitle={eventAgendaInfo.subtitle}
+									description={eventAgendaInfo.description}
+									backgroundColor={colors[index % colors.length]}
+									showBulletPoint
+								/>
+							</CardWrapper>
+						</Grid>
+					))}
+				</Grid>
+			</ContentContainer>
+		</EventAgendaContainer>
+	);
+};
+
+export default EventAgenda;
