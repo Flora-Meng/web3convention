764f9d34dd18f1218728322a32eb4c6659ff3c52
Flora <105186045+Flora-Meng@users.noreply.github.com>
2024-01-26 11:59:15 +1100

    kickoff to welcome

--- src/components/Pages/Home/HomeEvent/components/AgendaTable/AgendaTable.tsx
+++ src/components/Pages/Home/HomeEvent/components/AgendaTable/AgendaTable.tsx
@@ -104,23 +104,23 @@ const headConfig: {
 	title: string;
 	subtitle: string;
 }[] = [
-		{
-			title: WEEKDAY[5],
-			subtitle: '17 May'
-		},
-		{
-			title: WEEKDAY[6],
-			subtitle: '18 May'
-		},
-		{
-			title: WEEKDAY[0],
-			subtitle: '19 May'
-		},
-		{
-			title: WEEKDAY[1],
-			subtitle: '20 May'
-		}
-	];
+	{
+		title: WEEKDAY[5],
+		subtitle: '17 May'
+	},
+	{
+		title: WEEKDAY[6],
+		subtitle: '18 May'
+	},
+	{
+		title: WEEKDAY[0],
+		subtitle: '19 May'
+	},
+	{
+		title: WEEKDAY[1],
+		subtitle: '20 May'
+	}
+];
 
 const agendaConfig = [
 	{
