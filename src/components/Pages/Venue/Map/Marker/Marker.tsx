65940a944d4a0bd83df487fb23f3eae76d46f959
Darren Duan <tsdarren@gmail.com>
2024-02-18 22:38:11 +1100

    fix(venue page): fix the indentation bugs

--- src/components/Pages/Venue/Map/Marker/Marker.tsx
+++ src/components/Pages/Venue/Map/Marker/Marker.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 
 const Marker: React.FC<google.maps.MarkerOptions> = options => {
-  const [marker, setMarker] = useState<google.maps.Marker>();
-  const googleMapPrefix = 'https://www.google.com/maps?q=';
-  useEffect(() => {
-    if (!marker) {
-        setMarker(new google.maps.Marker());
-    }
-    return () => {
-		if (marker) {
-			marker.setMap(null);
-		}
-    };
+    const [marker, setMarker] = useState<google.maps.Marker>();
+    const googleMapPrefix = 'https://www.google.com/maps?q=';
+    useEffect(() => {
+        if (!marker) {
+            setMarker(new google.maps.Marker());
+        }
+        return () => {
+            if (marker) {
+                marker.setMap(null);
+            }
+        };
   }, [marker]);
 
   useEffect(() => {
